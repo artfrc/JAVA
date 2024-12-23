e2173c2ec75ac91eb7292e0504c0863b7b1223c0
Arthur <crfernandesarthur@gmail.com>
2024-12-23 10:14:13 -0300

    config: initialize first project

---
+++ first-project-spring/src/main/java/br/com/artfc/first_project_spring/FirstProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.artfc.first_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSpringApplication.class, args);
+	}
+
+}
---
+++ first-project-spring/src/test/java/br/com/artfc/first_project_spring/FirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.artfc.first_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
