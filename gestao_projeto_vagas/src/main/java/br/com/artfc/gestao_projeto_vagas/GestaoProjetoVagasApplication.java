7673b9ff75f67d2ec696943e64f573e3b9fa66af
Arthur <crfernandesarthur@gmail.com>
2024-12-26 14:03:08 -0300

    feat: Implement route to create candidates and also some validations

---
+++ gestao_projeto_vagas/src/main/java/br/com/artfc/gestao_projeto_vagas/GestaoProjetoVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.artfc.gestao_projeto_vagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProjetoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProjetoVagasApplication.class, args);
+	}
+
+}
---
+++ gestao_projeto_vagas/src/test/java/br/com/artfc/gestao_projeto_vagas/GestaoProjetoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.artfc.gestao_projeto_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoProjetoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
