d85072c7353fb2fa58fa3a31a11564cbe396374d
Arthur <crfernandesarthur@gmail.com>
2024-12-29 15:58:04 -0300

    feat: Implement JobEntity and use cases for jobs

--- gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/entities/JobEntity.java
@@ -24,7 +24,7 @@ public class JobEntity {
    private String benefits;
 
    @ManyToOne
-   @JoinColumn(name = "company_id")
+   @JoinColumn(name = "company_id", insertable = false, updatable = false)
    private CompanyEntity companyEntity;
 
    @Column(name = "company_id")
