3b36b5f9520455ff542ad1c6af50c45ab1134693
Arthur <crfernandesarthur@gmail.com>
2024-12-29 15:16:43 -0300

    feat: Implement company entity

--- gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -1,4 +1,4 @@
-package br.com.artfc.gestao_vagas.modules.candidate;
+package br.com.artfc.gestao_vagas.modules.company.entities;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -14,97 +14,76 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-@Entity(name = "candidate")
-public class CandidateEntity {
-   
+@Entity(name = "company")
+public class CompanyEntity {
+
    @Id
    @GeneratedValue(strategy = GenerationType.UUID)
    private UUID id;
 
-   private String name;
-
-   @NotBlank(message = "Username is required")
+   @NotBlank
    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Username should contain only letters and numbers")
    private String username;
 
-   @Email(message = "Email should be valid")
-   private String email;
-
    @Length(min = 6, max = 32, message = "Password should be between 6 and 32 characters")
    private String password;
+
+   @Email(message = "Email should be valid")
+   private String email;
+   private String website;
    private String description;
-   private String curriculum;
+   private String name;
 
    @CreationTimestamp
    private LocalDateTime createdAt;
-
-   public CandidateEntity(UUID id, String name, String username, String email, String password, String description, String curriculum) {
-      this.id = id;
-      this.name = name;
-      this.username = username;
-      this.email = email;
-      this.password = password;
-      this.description = description;
-      this.curriculum = curriculum;
+   
+   public LocalDateTime getCreatedAt() {
+      return createdAt;
    }
-
-   public CandidateEntity() {
+   public void setCreatedAt(LocalDateTime createdAt) {
+      this.createdAt = createdAt;
    }
-
    public UUID getId() {
       return id;
    }
-
    public void setId(UUID id) {
       this.id = id;
    }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
-
    public String getUsername() {
       return username;
    }
-
    public void setUsername(String username) {
       this.username = username;
    }
-
-   public String getEmail() {
-      return email;
-   }
-
-   public void setEmail(String email) {
-      this.email = email;
-   }
-
    public String getPassword() {
       return password;
    }
-
    public void setPassword(String password) {
       this.password = password;
    }
-
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      this.email = email;
+   }
+   public String getWebsite() {
+      return website;
+   }
+   public void setWebsite(String website) {
+      this.website = website;
+   }
    public String getDescription() {
       return description;
    }
-
    public void setDescription(String description) {
       this.description = description;
    }
-
-   public String getCurriculum() {
-      return curriculum;
+   public String getName() {
+      return name;
    }
-
-   public void setCurriculum(String curriculum) {
-      this.curriculum = curriculum;
+   public void setName(String name) {
+      this.name = name;
    }
-
+   
 }
