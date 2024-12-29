011a39cd9bb309610d0575ac6190beed19c6951d
Arthur <crfernandesarthur@gmail.com>
2024-12-29 16:17:14 -0300

    feat: Implement company controller

---
+++ gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -0,0 +1,34 @@
+package br.com.artfc.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.artfc.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.artfc.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+   @Autowired
+   private CreateCompanyUseCase createCompanyUseCase;
+
+   @PostMapping("/create")
+   public ResponseEntity<Object> createCompany(@Valid @RequestBody CompanyEntity company) {
+
+      try {
+         var result = this.createCompanyUseCase.execeute(company);
+
+         return ResponseEntity.ok().body(result);
+
+      }catch (Exception e) {
+         e.printStackTrace();
+         return ResponseEntity.badRequest().body(e.getMessage());
+      }
+   }
+}
