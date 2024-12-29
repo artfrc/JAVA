5ce6cdfc615739d6fec21b5664b44b289958098a
Arthur <crfernandesarthur@gmail.com>
2024-12-29 15:31:55 -0300

    feat:Implement job entity

---
+++ gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,84 @@
+package br.com.artfc.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "job")
+public class JobEntity {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.UUID)
+   private UUID id;
+   private String description;
+   private String level;
+   private String benefits;
+
+   @ManyToOne
+   @JoinColumn(name = "company_id")
+   private CompanyEntity companyEntity;
+
+   @Column(name = "company_id")
+   private UUID companyId;
+
+   @CreationTimestamp
+   private LocalDate createdAt;
+
+   public UUID getId() {
+      return id;
+   }
+
+   public void setId(UUID id) {
+      this.id = id;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getLevel() {
+      return level;
+   }
+
+   public void setLevel(String level) {
+      this.level = level;
+   }
+
+   public UUID getCompanyId() {
+      return companyId;
+   }
+
+   public void setCompanyId(UUID companyId) {
+      this.companyId = companyId;
+   }
+
+   public String getBenefits() {
+      return benefits;
+   }
+
+   public void setBenefits(String benefits) {
+      this.benefits = benefits;
+   }
+
+   public LocalDate getCreatedAt() {
+      return createdAt;
+   }
+
+   public void setCreatedAt(LocalDate createdAt) {
+      this.createdAt = createdAt;
+   }
+   
+}
