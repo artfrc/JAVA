c90968d7ba01eaf68e39d68935d3e526dd0e1328
Arthur <crfernandesarthur@gmail.com>
2025-01-08 08:47:05 -0300

    feat: implement hash password for candidate

--- gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ gestao_vagas/src/main/java/br/com/artfc/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.artfc.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.artfc.gestao_vagas.exceptions.UserFoundException;
@@ -13,6 +14,9 @@ public class CreateCandidateUseCase {
    @Autowired
    private CandidateRepository candidateRepository;
 
+   @Autowired
+   private PasswordEncoder passwordEncoder;
+
    public CandidateEntity execute(CandidateEntity candidate) {
       
       this.candidateRepository
@@ -20,6 +24,9 @@ public class CreateCandidateUseCase {
       .ifPresent((user) -> {
          throw new UserFoundException("User already exists");
       });
+
+      var password = passwordEncoder.encode(candidate.getPassword());
+      candidate.setPassword(password);
 
       return this.candidateRepository.save(candidate);
       
