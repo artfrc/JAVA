3eb98e0789f0872391d9dc4e1de6f352a0622a25
Arthur Fernandes Ribeiro Costa <crfernandesarthur@gmail.com>
2025-06-20 13:20:09 -0300

    Init project

--- Curso/src/main/java/com/remedios/artfrc/Curso/CursoApplication.java
+++ Curso/src/main/java/com/remedios/artfrc/Curso/CursoApplication.java
@@ -1,13 +1,13 @@
-package com.example.app;
+package com.remedios.artfrc.Curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class CursoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(CursoApplication.class, args);
 	}
 
 }
--- Curso/src/test/java/com/remedios/artfrc/Curso/CursoApplicationTests.java
+++ Curso/src/test/java/com/remedios/artfrc/Curso/CursoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.app;
+package com.remedios.artfrc.Curso;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppApplicationTests {
+class CursoApplicationTests {
 
 	@Test
 	void contextLoads() {
