931efe7b03849df5735520e3f0404413bb48c67a
Arthur Fernandes Ribeiro Costa <crfernandesarthur@gmail.com>
2025-06-24 10:21:36 -0300

    adicionando componentscan na classe de inicialização e um controller de teste.

---
+++ demo-multi-modulo/modulo-web/src/main/java/br/com/artfrc/demo/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package br.com.artfrc.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
