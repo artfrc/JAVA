55f78fc3f337528d424dd94498f7fe19ef40c5f6
ARTHUR RIBEIRO <crfernandesarthur@gmail.com>
2025-08-07 22:36:34 -0300

    sad but true

--- microservice/order/src/main/java/com/microservice/order/repository/OrdereItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.order.repository;
-
-import com.microservice.order.model.OrderedItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrdereItemRepository extends JpaRepository<OrderedItem, UUID> {
-}
