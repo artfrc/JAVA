e0b08c5432da9efd69c168d5343f786c8f4ab940
Arthur Fernandes Ribeiro Costa <crfernandesarthur@gmail.com>
2025-08-21 13:28:02 -0300

    complete config properties for DB

---
+++ demo/src/main/java/com/training/demo/model/Client.java
@@ -0,0 +1,36 @@
+package com.training.demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "Clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
+    public Client(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
